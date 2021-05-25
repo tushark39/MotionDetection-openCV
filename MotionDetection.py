c2c9654fe59f0e32a708022bdbb72022418afb41
Tushar <tushark39@gmail.com>
2021-05-26 00:59:55 +0530

    Indentation fixed

--- MotionDetection.py
+++ MotionDetection.py
@@ -4,11 +4,11 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 for _ in range(10):
     _, frame = cap.read()
-    frame = cv2.resize(frame, (640, 480))
-    frame = cv2.flip(frame, 1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (25, 25), 0)
-    last_frame = gray
+frame = cv2.resize(frame, (640, 480))
+frame = cv2.flip(frame, 1)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (25, 25), 0)
+last_frame = gray
      
     while True:
         _, frame = cap.read()
