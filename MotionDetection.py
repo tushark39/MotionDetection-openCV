b8ca171eb0236a85d887d672c8dbe417edb928a8
Tushar <tushark39@gmail.com>
2021-05-26 01:06:54 +0530

    Indentation Error fixed

--- MotionDetection.py
+++ MotionDetection.py
@@ -9,25 +9,23 @@ frame = cv2.flip(frame, 1)
 gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (25, 25), 0)
 last_frame = gray
-     
-    while True:
-        _, frame = cap.read()
-        frame = cv2.resize(frame, (640, 480))
-        frame = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (25, 25), 0)
-     
-        abs_diff = cv2.absdiff(last_frame, gray)
-        last_frame = gray
-        _, ad_mask = cv2.threshold(abs_diff, 15, 255, cv2.THRESH_BINARY)
-     
-        contours, _ = cv2.findContours(ad_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for contour in contours:
-            if cv2.contourArea(contour) < 1000:
-                continue
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.imshow("Abs diff mask", ad_mask)
-        cv2.imshow("Webcam", frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
+
+while True:
+    _, frame = cap.read()
+    frame = cv2.resize(frame, (640, 480))
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (25, 25), 0)
+    abs_diff = cv2.absdiff(last_frame, gray)
+    last_frame = gray
+    _, ad_mask = cv2.threshold(abs_diff, 15, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(ad_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow("Abs diff mask", ad_mask)
+    cv2.imshow("Webcam", frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
