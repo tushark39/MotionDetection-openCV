83954ebfa591d2898924ee2cd6ff417898ead270
Tushar <tushark39@gmail.com>
2021-05-26 00:56:11 +0530

    Bug Fixed

--- MotionDetection.py
+++ MotionDetection.py
@@ -1,9 +1,9 @@
-    import cv2
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    for _ in range(10):
-        _, frame = cap.read()
+import cv2
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+for _ in range(10):
+    _, frame = cap.read()
     frame = cv2.resize(frame, (640, 480))
     frame = cv2.flip(frame, 1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
